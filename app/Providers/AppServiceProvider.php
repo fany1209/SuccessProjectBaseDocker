6065e7cfd3e872395b92cbd50bcfba67c010df2c
fany1209 <stefanyhdz247@gmail.com>
2026-06-29 13:59:39 -0600

    migraciones y calidad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,12 +21,11 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
   
-public function boot(): void
-{
-    // Solo ejecutar esto si NO estamos en la consola (Artisan)
-    if (! app()->runningInConsole()) {
-        $pendingRequests = \App\Models\LotRequest::where('status', 'pendiente')->count();
-        \Illuminate\Support\Facades\View::share('pendingRequests', $pendingRequests);
-    }
-}
+    public function boot()
+        {
+            View::share(
+                'pendingLotRequests',
+                LotRequest::where('status', 'pendiente')->count()
+            );
+        }
 }
