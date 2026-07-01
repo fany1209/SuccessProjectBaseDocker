dba5c6cd6166deb4b803edf6196e7904845e4b89
fany1209 <stefanyhdz247@gmail.com>
2026-07-01 12:10:13 -0600

    cambios web

--- routes/web.php
+++ routes/web.php
@@ -200,7 +200,7 @@ Route::middleware([
 
     Route::get('suppliers-get-directory', [DirectoryController::class, 'getDirectory'])->name('suppliers.getDirectory');
     Route::resource('purchases/supplier-directory', DirectoryController::class)
-    ->names('supplier_directory') // Las rutas serán: supplier_directory.index, supplier_directory.store...
+    ->names('supplier_directory')
     ->parameters(['supplier-directory' => 'code']);
 
     //Propects
@@ -489,7 +489,7 @@ Route::prefix('laboratory/materials')->group(function () {
     Route::get('/rh/asistencia', [RhController::class, 'index'])->name('attendance.index');
     Route::post('/rh/asistencia/upload', [RhController::class, 'uploadCsv'])->name('attendance.upload');
 
-
+    
     // La ruta de la vista principal
     Route::get('/recursos-humanos', [RecursosHumanosController::class, 'index'])->name('expediente.index');
 
@@ -500,15 +500,9 @@ Route::prefix('laboratory/materials')->group(function () {
     Route::post('/rh/vacaciones/generar-pdf', [RecursosHumanosController::class, 'generarPdfVacaciones'])->name('rh.vacaciones.pdf');
     Route::post('/rh/dnc/generar-pdf', [RecursosHumanosController::class, 'generarPdfDnc'])->name('rh.dnc.pdf');
     Route::post('/rh/practicantes/generar-pdf', [RecursosHumanosController::class, 'generarExpedientePracticantePdf'])->name('rh.practicantes.pdf');
-
-    // Ruta para generar el PDF de la Evaluación del Desempeño
-    Route::post('/rh/evaluacion-desempeno/pdf', [App\Http\Controllers\RecursosHumanosController::class, 'evaluacionDesempenoPdf'])->name('rh.evaluacion_desempeno.pdf');
-
-    // Ruta para generar el PDF de Solicitud de Personal
-    Route::post('/rh/solicitud-personal/pdf', [App\Http\Controllers\RecursosHumanosController::class, 'solicitudPersonalPdf'])->name('rh.solicitud_personal.pdf');
-
-    // Ruta para generar el PDF de Convenio con Instituciones
-    Route::post('/rh/convenio-instituciones/pdf', [App\Http\Controllers\RecursosHumanosController::class, 'convenioInstitucionesPdf'])->name('rh.convenio_instituciones.pdf');
+    Route::post('/rh/evaluacion-desempeno/pdf', [RecursosHumanosController::class, 'evaluacionDesempenoPdf'])->name('rh.evaluacion_desempeno.pdf');
+    Route::post('/rh/solicitud-personal/pdf', [RecursosHumanosController::class, 'solicitudPersonalPdf'])->name('rh.solicitud_personal.pdf');
+    Route::post('/rh/convenio-instituciones/pdf', [RecursosHumanosController::class, 'convenioInstitucionesPdf'])->name('rh.convenio_instituciones.pdf');
 
     //documents
     Route::get('/delivery-note/{sale_id}', [PdfController::class, 'makeDeliveryNotePDF'])
